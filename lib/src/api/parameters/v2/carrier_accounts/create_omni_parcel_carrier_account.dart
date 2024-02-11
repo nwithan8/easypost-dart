6711c25d2ccda989613f5ffea02a351f5f054eea
Nate Harris <nwithan8@users.noreply.github.com>
2024-02-11 16:23:23 -0700

    - More carrier account parameters

---
+++ lib/src/api/parameters/v2/carrier_accounts/create_omni_parcel_carrier_account.dart
@@ -0,0 +1,27 @@
+import 'package:easypost/src/api/parameters/v2/carrier_accounts/create_carrier_account.dart';
+import 'package:easypost/src/enums/carrier_account_type.dart';
+import 'package:easypost/src/internal/parameter_annotation.dart';
+import 'package:easypost/src/internal/reflection.dart';
+
+@reflector
+class CreateOmniParcelCarrierAccount extends CreateCarrierAccount {
+  @JsonParameter(
+      Necessity.required, ['carrier_account', "credentials", "api_access_key"])
+  String? apiAccessKey;
+
+  @JsonParameter(
+      Necessity.required, ['carrier_account', "credentials", "return_api_access_key"])
+  String? returnApiAccessKey;
+
+  @JsonParameter(
+      Necessity.required, ['carrier_account', "test_credentials", "api_access_key"])
+  String? testApiAccessKey;
+
+  @JsonParameter(
+      Necessity.required, ['carrier_account', "test_credentials", "return_api_access_key"])
+  String? testReturnApiAccessKey;
+
+  CreateOmniParcelCarrierAccount()
+      : super(CarrierAccountType.omniParcel,
+            defaultCarrierAccountCreationEndpoint);
+}
